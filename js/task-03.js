488995a8483f9bc07909209f4762ab56ce42772b
Yevhen Danilov <danilov.evgen@gmail.com>
2021-08-26 23:02:02 +0300

    hw 06 _1_2_3

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,38 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
+  {
+    url: 'https://picsum.photos/300/200?random=1',
+    alt: 'Random picture',
+  },
+  {
+    url: 'https://picsum.photos/300/200?random=2',
+    alt: 'Random picture',
+  },
+  {
+    url: 'https://picsum.photos/300/200?random=3',
+    alt: 'Random picture',
+  },
+  {
+    url: 'https://picsum.photos/300/200?random=4',
+    alt: 'Random picture',
+  },
+  {
+    url: 'https://picsum.photos/300/200?random=5',
+    alt: 'Random picture',
+  },
+  {
+    url: 'https://picsum.photos/300/200?random=6',
+    alt: 'Random picture',
+  },
 ];
+
+
+const gallery = document.querySelector(".gallery");
+
+const markup = images
+  .map((image) => `<li><img src=${image.url} alt=${image.alt}></li>`)
+  .join("");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+
