61cba43148306d355e11bd53c12fdb5df296ebe4
Yevhen Danilov <danilov.evgen@gmail.com>
2021-08-30 17:42:58 +0300

    task 9 upd

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
-const changeColorBtn = document.querySelector(".change-color");
+const changeColor = document.querySelector(".color");
 const bodyTarg = document.querySelector("body");
+const  changeColorBtn = document.querySelector(".change-color");
 
 changeColorBtn.addEventListener("click", getRandomHexColor);
 
 function getRandomHexColor(event) {
   let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   bodyTarg.style.backgroundColor = color;
-  changeColorBtn.textContent = `Selected color: ${color}`;
+  changeColor.textContent = `Selected color: ${color}`;
 
 }
  
