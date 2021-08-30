abac2685da691e46df32bae8258f9e20967d4c76
Yevhen Danilov <danilov.evgen@gmail.com>
2021-08-30 23:25:59 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,97 +5,44 @@ const dataDestroyBtn = document.querySelector('[data-destroy]');
 const input = document.querySelector("input");
 
 
-// Запуск створення блоків по кліку на відповідну кнопку
-dataCreateBtn.addEventListener("click", createBoxes);
-
-input.addEventListener("input", (event) => {
-  const amount = event.currentTarget.value;
-});
-
-
-if (input.textContent === "") => {return ``;};
-
-// let amount = 20;
-// console.log(input.getValue());
-
-// let amount = input.value();
-
-function createBoxes(amount) {
-
-      // Область кліку = кнопка
-
-      // if (dataCreateBtn.target.nodeName !== "BUTTON") {
-      //   return;
-      // }
-
-      // 
-      const boxes = [];
-
-      for (let i = 1; i <= amount; i++) {
-        const bgColor = getRandomHexColor();
-        const box = document.createElement("div");
-        // box.style.display = flex;
-        // box.style.flexDirection = column;
-        box.style.backgroundColor = bgColor;
-        box.style.width += 10;
-        box.style.height += 10;
-        boxes.push(box);
-      }
-  
-      // console.log(...boxes);
-
-  }
-
-
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
+function createBoxes(amount) {
 
-// ------------------------
+  amount = input.value;
+  const boxesArr = [];
+  // if (amount > 100) {alert ("Insert number from 1 to 100"); return};
 
-// const colorPalette = document.querySelector(".color-palette");
-// const output = document.querySelector(".output");
 
-// colorPalette.addEventListener("click", selectColor);
+      for (let i = 0; i < amount; i++) {
+        const bgColor = getRandomHexColor();
+        const box = document.createElement("div");
+        box.style.backgroundColor = bgColor;
+        box.style.width = 30 + i*10 + "px";
+        box.style.height = 30 + i*10 + "px";
+        boxesArr.push(box);
+      }
 
-// // This is where delegation «magic» happens
-// function selectColor(event) {
-//   if (event.target.nodeName !== "BUTTON") {
-//     return;
-//   }
+      boxes.append(...boxesArr);
+      // console.log(boxes);
 
-//   const selectedColor = event.target.dataset.color;
-//   output.textContent = `Selected color: ${selectedColor}`;
-//   output.style.color = selectedColor;
-// }
+  }
 
-// // Some helper functions to render palette items
-// createPaletteItems();
 
-// function createPaletteItems() {
-//   const items = [];
-//   for (let i = 0; i < 60; i++) {
-//     const color = getRangomColor();
-//     const item = document.createElement("button");
-//     item.type = "button";
-//     item.dataset.color = color;
-//     item.style.backgroundColor = color;
-//     item.classList.add("item");
-//     items.push(item);
-//   }
+// Створення блоків по кліку на відповідну кнопку
+dataCreateBtn.addEventListener("click", createBoxes);
 
-//   colorPalette.append(...items);
-// }
 
-// function getRangomColor() {
-//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-// }
+function destroyBoxes() {
+   boxes.innerHTML = "";
+  //  console.log(boxes);
 
-// function getRandomHex() {
-//   return Math.round(Math.random() * 256)
-//     .toString(16)
-//     .padStart(2, "0");
-// }
+}
+
+// Видалення блоків по кліку на відповідну кнопку
+dataDestroyBtn.addEventListener("click", destroyBoxes);
+
+
